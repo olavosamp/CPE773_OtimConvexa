6a856afbcbca952be4e24a96441d1b30456e926c
Olavo <olavosamp@poli.ufrj.br>
2018-10-09 19:43:23 -0300

    Added Fibonacci search, still WIP

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class OptimizationAlgorithm:
+class LineSearch:
     def __init__(self, costFunc, xtol):
         self.costFunc = costFunc
         self.fevals = 0
@@ -11,7 +11,7 @@ class OptimizationAlgorithm:
         self.fevals += 1
         return result
 
-class DichotomousSearch(OptimizationAlgorithm):
+class DichotomousSearch(LineSearch):
     def __init__(self, costFunc, interval, xtol=1e-8, maxIters=1e3):
         super().__init__(costFunc, xtol)
 
@@ -45,5 +45,96 @@ class DichotomousSearch(OptimizationAlgorithm):
             self.iteration()
             self.numIters += 1
 
-        xOpt = self.interval[0]
-        return xOpt
+        self.xOpt = self.interval[0]
+        return self.xOpt
+
+# Fibonacci Search
+class FibonacciSearch(LineSearch):
+    def __init__(self, costFunc, interval, xtol=1e-8, maxIters=1e3):
+        super().__init__(costFunc, xtol)
+
+        self.maxIters = maxIters
+        self.epsilon = xtol/4
+        self.interval = interval
+
+        # Compute number of iterations required to reach epsilon
+        self.compute_fibonacci(40) # Long Fibonacci will overflow and break the algorithm
+        auxSeq = self.fibSequence[(self.fibSequence < 1/self.epsilon)]
+
+        self.iterations = np.argmax(auxSeq)
+        if self.iterations > self.maxIters:
+            self.iterations = self.maxIters
+
+        self.fibSequence = self.fibSequence[:self.iterations]
+
+    def compute_fibonacci(self, size):
+        self.fibSequence = np.zeros(size, dtype=np.int)
+        self.fibSequence[0] = 1
+        self.fibSequence[1] = 1
+        for i in range(2, size):
+            self.fibSequence[i] = self.fibSequence[i-1] + self.fibSequence[i-2]
+
+        return self.fibSequence
+
+    def optimize(self):
+        # Initialize variables
+        fA = np.zeros(self.iterations)
+        fB = np.zeros(self.iterations)
+        xA = np.zeros(self.iterations)
+        xB = np.zeros(self.iterations)
+        xU = np.zeros(self.iterations)
+        xL = np.zeros(self.iterations)
+        I  = np.zeros(self.iterations+1)
+
+        xL[0] = self.interval[0]
+        xU[0] = self.interval[1]
+
+        k = 0 # Iteration counter
+        I[0] = xU[0] - xL[0]
+        I[1] = I[0]*self.fibSequence[self.iterations-2]/self.fibSequence[self.iterations-1]
+
+        xA[0] = xU[0] - I[1]
+        xB[0] = xL[0] - I[1]
+
+        # Evaluate f(xA), f(xB)
+        fA[0] = self.evaluate(xA[0])
+        fB[0] = self.evaluate(xB[0])
+
+        # print("\nStart iterating")
+        while True:
+            print("Iter: ", k)
+            I[k+2] = I[k+1]*self.fibSequence[self.iterations - k - 2]/ self.fibSequence[self.iterations - k-1]
+
+            if fA[k] >= fB[k]:
+                xL[k+1] = xA[k]
+                xU[k+1] = xU[k]
+                xA[k+1] = xB[k]
+                xB[k+1] = xA[k] + I[k+2]
+
+                fA[k+1] = fB[k]
+                fB[k+1] = self.evaluate(xB[k+1])
+            elif fA[k] < fB[k]:
+                xL[k+1] = xL[k]
+                xU[k+1] = xB[k]
+                xA[k+1] = xU[k+1] - I[k+2]
+                xB[k+1] = xA[k]
+
+                fA[k+1] = self.evaluate(xA[k+1])
+                fB[k+1] = fA[k]
+
+            if (k == self.iterations-2) or (xA[k+1] > fB[k+1]):
+                self.xOpt = xA[k+1]
+                return self.xOpt
+            else:
+                k += 1
+
+
+# Golden Section Search
+
+# Quadratic interpolation method
+
+# Cubic interpolation method
+
+# Davies-Swann-Campey Algorithm
+
+# Inexact Line Search
--- test.py
+++ test.py
@@ -1,15 +1,32 @@
 import numpy as np
 
-from algorithms import DichotomousSearch
+from algorithms import DichotomousSearch, FibonacciSearch
 from functions import poly1, quadratic
 
-interval = [-0.5, 0.5]
-maxIters = 1000
-xtol = 1e-5
-alg = DichotomousSearch(poly1, interval, xtol=xtol, maxIters=maxIters )
+def print_results(alg, string):
+    print(string)
+    xOpt = alg.optimize()
+    print("x* = ", xOpt)
+    print("Fevals: ", alg.fevals)
+    print("xtol: ", alg.xtol)
 
-print("\nOptimize f(x) = −5x5 + 4x4 − 12x3 + 11x2 − 2x + 1\n")
-xOpt = alg.optimize()
-print("x* = ", xOpt)
-print("Fevals: ", alg.fevals)
-print("xtol: ", alg.xtol)
+
+xtol = 1e-5
+maxIters = 1000
+string = "\nOptimize f(x) = −5x5 + 4x4 − 12x3 + 11x2 − 2x + 1\nSolution x = 0.10986"
+
+# Dichotomous Search
+interval = [-0.5, 0.5]
+dichot = DichotomousSearch(poly1, interval, xtol=xtol, maxIters=maxIters )
+print_results(dichot, "\nDichotomous Search"+string)
+
+# Fibonacci Search
+# interval = [-0.5, 0.5]
+fib = FibonacciSearch(poly1, interval, xtol=xtol, maxIters=maxIters )
+# print(fib.compute_fibonacci(10))
+print_results(fib, "\nFibonacci Search"+string)
+
+# Algorithm
+# interval = [,]
+# alg = ALGCLASS(poly1, interval, xtol=xtol, maxIters=maxIters )
+# print_results(alg, string)
