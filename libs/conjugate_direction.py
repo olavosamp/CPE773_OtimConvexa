Fixed ConjugateGradient bug when gradient is 0

libs/conjugate_direction.py
@@ -80,8 +80,6 @@ class ConjugateGradient(ConjugateDirection):
 
 
     def line_search(self):
-        self.alpha[self.iter] = (np.transpose(self.g[self.iter]) @ self.g[self.iter])/(np.transpose(self.direction[self.iter]) @ self.hessVal[self.iter] @ self.direction[self.iter])
-
         self.xLS = self.x[self.iter] + self.alpha[self.iter]*self.direction[self.iter]
         return self.xLS
 
@@ -92,12 +90,26 @@ class ConjugateGradient(ConjugateDirection):
         # Initial values for g and d
         self.g[0] = self.b + self.H @ self.x[0]
         self.direction[0] = -self.g[0]
-
         while self.iter < self.maxIters-2:
             self.hessVal[self.iter] = self.hessFunc(self.x[self.iter])
-            self.alpha[self.iter] = (np.transpose(self.g[self.iter]) @ self.g[self.iter])/(np.transpose(self.direction[self.iter]) @ self.hessVal[self.iter] @ self.direction[self.iter])
 
-            self.x[self.iter+1] = self.line_search()
+            # Check if gradient is zero. If true, skip to stopping condition check
+            if not (self.g[self.iter] == 0).all():
+                self.alpha[self.iter] = (np.transpose(self.g[self.iter]) @ self.g[self.iter])/ \
+                (np.transpose(self.direction[self.iter]) @ self.hessVal[self.iter] @ self.direction[self.iter])
+
+                self.x[self.iter+1] = self.line_search()
+            else:
+                self.x[self.iter+1] = self.x[self.iter]
+
+            ## Debug
+            # p|rint("\nIter", self.iter)
+            # print("hessVal:\n", self.hessVal[self.iter])
+            # print("g:\t", self.g[self.iter])
+            # print("direction: ", self.direction[self.iter])
+            # print("alpha[k]: ", self.alpha[self.iter])
+            # print("x[k+1]: ", self.x[self.iter+1])
+            # input()
 
             # Check stopping condition
             if self.stopping_cond() == True:
@@ -107,12 +119,12 @@ class ConjugateGradient(ConjugateDirection):
 
             # Compute new direction
             self.direction[self.iter+1] = self.get_direction()
-
             self.iter += 1
 
         print("\nAlgorithm did not converge.")
         self.xOpt = self.x[-1]
         return self.xOpt, self.costFunc(self.x[-1]), self.fevals
+
 
 class FletcherReeves(ConjugateGradient):
     def __init__(self, func, initialX, interval=[-1e15, 1e15], ftol=1e-6, maxIters=1e3, maxItersLS=200):

libs/utils.py
@@ -5,7 +5,7 @@ import autograd.numpy as np
 #   f_i <= 0
 # should be used.
 def modified_log(x):
-    if (x < 0).any():   # Possibly change to x <= 0
-        return np.inf
+    if (x <= 0).any():   # Option between x <= 0 and x < 0
+        return -np.inf
     else:
         return np.sum(np.log(x))

lista4/L4_Q1.py
@@ -75,16 +75,16 @@ for initialX in initialXList:
     # print("Delta f(x) = ", deltaF)
     # print("Ref FEvals = ", fevalRef)
 
-    # F, x_hat = eq_constraint_elimination(function, eqConstraintsMat)
-    # newFunction = compose_eq_cons_func(function, F, x_hat)
-    #
-    # newConstraintList = get_scipy_constraints(None, ineqConstraints)
-    #
-    #
-    # optimResult = spo.minimize(newFunction, np.array([0, 1]), method='SLSQP', tol=ftol)
-    #                             # constraints=newConstraintList)
-    # xOpt = F @ optimResult.x + x_hat
-    # fOpt = function(xOpt)
+    F, x_hat = eq_constraint_elimination_composer(eqConstraintsMat)
+    newFunction = compose_eq_cons_func(function, F, x_hat)
+
+    newConstraintList = get_scipy_constraints(None, ineqConstraints)
+
+
+    optimResult = spo.minimize(newFunction, np.array([0, 1]), method='SLSQP', tol=ftol)
+                                # constraints=newConstraintList)
+    xOpt = F @ optimResult.x + x_hat
+    fOpt = function(xOpt)
 
     print("\nConstrained Optimization")
     # print("F:", F)

lista4/testBarrierMethod.py
@@ -42,16 +42,18 @@ eqConstraintsMat = {'A': np.array([[1, 2, 1, 2],
 #   f(x) <= 0
 # As such, they are defined in Boyd's format and converted by
 # get_scipy_constraints script
-ineqConstraints = [ lambda x: -x,
+ineqConstraints = [ lambda x: x,
 ]
 
 constraintList = get_scipy_constraints(eqConstraintsFun, ineqConstraints)
 
 initialX = np.array([1,0,0,1])
-# initialX = feasibility(constraintList, initialX)
+initialX = feasibility(constraintList, initialX)
+
+print("Initial X:\n", initialX)
 
 print("Starting optimization")
-xOpt, fOpt = barrier_method(function, constraintList, initialX, interval=interval,
+xOpt, fOpt = barrier_method(function, constraintList, eqConstraintsMat, initialX, interval=interval,
                             ftol=ftol, maxIters=maxIters, maxItersLS=maxItersLS)
 
 print("\nConstrained Optimization")